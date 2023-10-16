0f559b13e5b903f1137c2d073a614f3121134acc
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-10-16 09:25:43 -0400

    erffdf

--- fart.js
+++ fart.js
@@ -6,5 +6,6 @@ const DOMSelectors = {
 DOMSelectors.form.addEventListener('submit', function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2s.forEach((el)=> el.textContent = DOMSelectors.firstName.value);
+    DOMSelectors.h2s.forEach((el)=>
+        el.textContent = DOMSelectors.firstName.value);
 });
