d78fc89d171bfd11e7b10e209c70af29e853f969
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-10-19 09:26:02 -0400

    d

--- fart.js
+++ fart.js
@@ -1,11 +1,15 @@
 const DOMSelectors = {
-    form: document.querySelector('#form'),
-    firstName: document.querySelector('.first-name'),
-    h2s: document.querySelectorAll('h2')
+    form: document.querySelector('#form-first'),
+    postit: document.querySelector('.post-it'),
+    flexblox: document.querySelector('.flexblox'),
+    posttext: document.querySelector('.post-text'),
+    input1: document.getElementById('input-1')  
 }
+
+console.log(DOMSelectors.input1)
+console.log( DOMSelectors.flexblox)
 DOMSelectors.form.addEventListener('submit', function(event){
     event.preventDefault();
-    console.log(DOMSelectors.firstName.value);
-    DOMSelectors.h2s.forEach((el)=>
-        el.textContent = DOMSelectors.firstName.value);
+    DOMSelectors.posttext.insertAdjacentHTML(DOMSelectors.postit, DOMSelectors.input1.value)
+    //insert adjacent html
 });
