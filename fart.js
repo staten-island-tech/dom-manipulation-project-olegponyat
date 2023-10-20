4a67afa17f63b159f0efa620af2090ba73b856e7
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-10-20 09:25:32 -0400

    dd

--- fart.js
+++ fart.js
@@ -2,14 +2,24 @@ const DOMSelectors = {
     form: document.querySelector('#form-first'),
     postit: document.querySelector('.post-it'),
     flexblox: document.querySelector('.flexblox'),
+    posttitle: document.querySelector('.post-title'),
     posttext: document.querySelector('.post-text'),
-    input1: document.getElementById('input-1')  
+    inputs: document.querySelectorAll('input'),
+    button: document.querySelector('.remove')
 }
-
-console.log(DOMSelectors.input1)
-console.log( DOMSelectors.flexblox)
-DOMSelectors.form.addEventListener('submit', function(event){
-    event.preventDefault();
-    DOMSelectors.posttext.insertAdjacentHTML(DOMSelectors.postit, DOMSelectors.input1.value)
-    //insert adjacent html
-});
+console.log(DOMSelectors.button)
+function addPostIt(){
+    DOMSelectors.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        DOMSelectors.flexblox.insertAdjacentHTML("afterbegin", "<div class=post-it></div>")
+        
+    });
+}
+function removePostIt(){
+    DOMSelectors.button.addEventListener('click', function(event){
+        event.preventDefault();
+        DOMSelectors.postit.remove();
+    })
+}
+addPostIt();
+removePostIt();
